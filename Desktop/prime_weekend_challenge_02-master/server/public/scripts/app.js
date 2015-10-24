6de633cba7efd3213856b9568c3de51392ba9e51
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-10-24 10:06:38 -0500

    10am save

--- Desktop/prime_weekend_challenge_02-master/server/public/scripts/app.js
+++ Desktop/prime_weekend_challenge_02-master/server/public/scripts/app.js
@@ -7,22 +7,45 @@ $(document).ready(function(){
         type : "GET",
         url  : "/data",
         success : function(zeta) {
-        console.log(zeta);
-            appendDom(zeta);
+            unwrapData(zeta);
         }
 
     });
+// variables
+//var arrayOfPeers=[];
 
 
+//page call functions
 
 
 });
 
-function appendDom(data){
+//functions
+
+
+function unwrapData(data){
 
 for (var i = 0; i < data.zeta.length; i++){
-    var arrayOfPeers = data.zeta[i];
-    //console.log("array of peers ",arrayOfPeers);
-    //console.log("array of peers 5th position",arrayOfPeers[4]);
+    var arrayOfPeers[] = data.zeta[i];
     }
+   console.log("array of peers in unwrap",arrayOfPeers);
+    createCarousel(arrayOfPeers);
+    return;
 }
+
+function createCarousel(array){
+for (var i = 0; i < array.length;i++) {
+
+        $('.main').append("<div class='person' id='" + i + "'</div>")
+        $('.person').last().append("<h1>"+array.name+"</h1>");
+        $('.person').last().append("<h5>"+array.github+"</h5>");
+        $('.person').last().append("<p1>"+array.shoutout+"</p1>");
+    console.log("working");
+
+    }
+
+}
+
+
+
+
