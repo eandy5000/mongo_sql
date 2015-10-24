9739ef132849038de91a309d412a569315f9ca00
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-10-24 12:25:56 -0500

    midday

--- Desktop/prime_weekend_challenge_02-master/server/public/scripts/app.js
+++ Desktop/prime_weekend_challenge_02-master/server/public/scripts/app.js
@@ -1,3 +1,7 @@
+// variables
+var indexTracker = 0;
+var people;
+
 $(document).ready(function(){
 
 
@@ -7,16 +11,26 @@ $(document).ready(function(){
         type : "GET",
         url  : "/data",
         success : function(zeta) {
+            people = zeta;
             unwrapData(zeta);
+
         }
 
     });
-// variables
-//var arrayOfPeers=[];
+
+
+
 
 
 //page call functions
+    updateIndexPoints();
+    createNavButtons();
 
+
+//listeners
+    $("#prev").on('click', prevSlide);
+    $("#next").on('click', nextSlide);
+    console.log("index tracker ", indexTracker);
 
 });
 
@@ -26,26 +40,67 @@ $(document).ready(function(){
 function unwrapData(data){
 
 for (var i = 0; i < data.zeta.length; i++){
-    var arrayOfPeers[] = data.zeta[i];
-    }
-   console.log("array of peers in unwrap",arrayOfPeers);
-    createCarousel(arrayOfPeers);
-    return;
-}
-
-function createCarousel(array){
-for (var i = 0; i < array.length;i++) {
-
-        $('.main').append("<div class='person' id='" + i + "'</div>")
-        $('.person').last().append("<h1>"+array.name+"</h1>");
-        $('.person').last().append("<h5>"+array.github+"</h5>");
-        $('.person').last().append("<p1>"+array.shoutout+"</p1>");
-    console.log("working");
+    var arrayOfPeers = data.zeta[i];
+    $('.main').append("<div class='person' id='" + i + "'</div>")
+    $('.person').last().append("<h1>"+arrayOfPeers.name+"</h1>");
+    $('.person').last().append("<h5>"+arrayOfPeers.github+"</h5>");
+    $('.person').last().append("<p1>"+arrayOfPeers.shoutout+"</p1>");
 
     }
+//unwrap end below
+}
+
+function updateIndexPoints(){
+
+
+
+//updateIndex ends below
+}
+
+function createNavButtons(){
+
+    $('.main').parent().append("<div class='navigation'></div>");
+    $('.navigation').last().append("<div class='nav' id='prev'>Previous</div> ");
+    $('.navigation').last().append("<div class='nav' id='next'>Next</div>");
+
 
 }
 
+function nextSlide(){
 
+    incrementTracker();
+    console.log("index tracker ", indexTracker);
 
+   //nextslide ends below
+}
+
+function prevSlide(){
+    decrementTracker();
+    console.log("index tracker ", indexTracker);
+    //prevslide ends below
+}
+
+function incrementTracker(){
+    var sliderCount = people.length;
+    var newTracker = (indexTracker + 1) % sliderCount;
+    indexTracker = newTracker;
+
+    console.log("index tracker in + ", indexTracker);
+    //increment tracker ends below
+}
+
+function decrementTracker(){
+
+    var sliderCount = people.length;
+    var newTracker = (indexTracker - 1) % sliderCount;
+
+    if (newTracker < 0) {
+        newTracker = sliderCount - 1;
+    }
+
+    indexTracker = newTracker;
+    console.log("index tracker in - ", indexTracker);
+
+    //decrement tracker ends below
+}
 
